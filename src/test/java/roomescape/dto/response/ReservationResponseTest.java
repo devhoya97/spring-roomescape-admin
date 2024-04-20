08fe731c31ebbc1242d70b1d304d5ab8e47a48f7
devhoya97 <diajbh123@gmail.com>
2024-04-20 22:20:08 +0900

    test: 여러 번 사용되는 상수를 변수로 빼서 가독성 향상

--- src/test/java/roomescape/dto/response/ReservationResponseTest.java
+++ src/test/java/roomescape/dto/response/ReservationResponseTest.java
@@ -14,14 +14,11 @@ class ReservationResponseTest {
     @DisplayName("id와 Reservation 객체로 ReservationResponse를 만든다.")
     void createReservationResponse() {
         Long id = 1L;
-        Reservation reservation = new Reservation("brown",
-                LocalDate.of(2024, 4, 1),
-                LocalTime.of(15, 32));
-        ReservationResponse expected = new ReservationResponse(
-                1L,
-                "brown",
-                LocalDate.of(2024, 4, 1),
-                LocalTime.of(15, 32));
+        String name = "브라운";
+        LocalDate date = LocalDate.parse("2024-04-01");
+        LocalTime time = LocalTime.parse("07:00");
+        Reservation reservation = new Reservation(name, date, time);
+        ReservationResponse expected = new ReservationResponse(id, name, date, time);
 
         ReservationResponse reservationResponse = ReservationResponse.of(id, reservation);
 
